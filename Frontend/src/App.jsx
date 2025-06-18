db7c47b03dba9c62ab2538d69ba22e2cd800b98b
Maherisoa Nosilala <nosilala13@gmail.com>
2025-06-18 14:28:15 +0300

    Configuration du layout

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 
-import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Layout from './pages/Layout/Layout'
 
 function App() {
 
@@ -9,7 +9,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} /> 
+        <Route path="/" element={<Home />} />
+        <Route path='/layout' element={<Layout />} />
+
       </Routes>
     </BrowserRouter>
   )
---
+++ Frontend/src/pages/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { TopBar } from '../../components/TopBar/TopBar'
+import SideBar from '../../components/SideBar/SideBar'
+
+const Layout = () => {
+  return (
+    <div>
+      <TopBar/>
+       <SideBar/>
+    </div>
+  )
+}
+
+export default Layout
