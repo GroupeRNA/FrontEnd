6876111002e9a56ba8dd741f872e5ee6ad9b4553
tovogilbert <tovogilbert283@gmail.com>
2025-06-29 09:04:55 +0300

    Endpoints

---
+++ Frontend/src/data/constants/endpoints.js
@@ -0,0 +1,13 @@
+
+//Les endpoints 
+
+export const CREATE_AUDIO = 'create_audio/';
+export const LIST_AUDIO = 'list_audio/';
+export const GET_AUDIO = 'get_audio/';
+export const DELETE_AUDIO = 'delete_audio/';
+
+export const CREATE_TRANSCRIPTION = 'create_transcription/';
+export const LIST_TRANSCRIPTION = 'list_transcription/';
+export const GET_TRANSCRIPTION = 'get_transcription/';
+export const UPDATE_TRANSCRIPTION = 'update_transcription/';
+export const DELETE_TRANSCRIPTION = 'delete_transcription/';
