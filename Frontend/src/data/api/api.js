ac913cbb2d9de5ad6e840a44fb835fe51fbfdd98
tovogilbert <tovogilbert283@gmail.com>
2025-06-29 08:59:37 +0300

    Cnnfiguration API

---
+++ Frontend/src/data/api/api.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:8000';
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Intercepteur pour les requêtes
+api.interceptors.request.use(
+  (config) => {
+    // Headers ici (comme les tokens d'authentification)
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Intercepteur pour les réponses
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    // Gestion centralisée des erreurs
+    console.error('API Error:', error.response);
+    return Promise.reject(error);
+  }
+);
+
+export default api;
